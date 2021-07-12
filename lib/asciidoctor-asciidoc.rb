592922a777382987e59a3adb7dd9ff03409ca4ee
Pawel Veselov <pawel.veselov@gmail.com>
2021-07-12 18:06:19 +0200

    Clean up document attribute conversion

--- lib/asciidoctor-asciidoc.rb
+++ lib/asciidoctor-asciidoc.rb
@@ -4,12 +4,20 @@ require 'asciidoctor/converter'
 
 class AsciiDoctorAsciiDocConverter < Asciidoctor::Converter::Base
 
+  MY_BACKEND = "asciidoc"
+  MY_FILETYPE = "asciidoc"
+  MY_EXTENSION = ".adoc"
+
   ATTR_ID = "id"
   ATTR_ROLE = "role"
   ATTR_TITLE = "title"
   ATTR_WINDOW = "window"
   ATTR_OPTS = "opts"
   ATTR_DOC_FILE = "docfile"
+  ATTR_DOC_TITLE = "doctitle"
+  ATTR_DOC_TYPE = "doctype"
+  ATTR_ICONS_DIR = "iconsdir"
+  ATTR_IMAGES_DIR = "imagesdir"
 
   ROLE_BARE = "bare"
 
@@ -19,19 +27,79 @@ class AsciiDoctorAsciiDocConverter < Asciidoctor::Converter::Base
 
   ANCHOR_ATTRIBUTES = [ATTR_ID, ATTR_ROLE, ATTR_WINDOW, ATTR_OPTS].to_set
 
-  register_for :asciidoc
+  # https://docs.asciidoctor.org/asciidoc/latest/attributes/document-attributes-reference/
+  INTRINSIC_DOC_ATTRIBUTES = %w(
+    backend basebackend docdate docdatetime docdir docfile
+    docfilesuffix docname doctime docyear embedded filetype
+    htmlsyntax localdate localdatetime localtime localyear
+    outdir outfile outfilesuffix safe-mode-level safe-mode-name
+    safe-mode-unsafe safe-mode-safe safe-mode-server safe-mode-secure user-home
+    asciidoctor asciidoctor-version authorcount
+  ).to_set
+  # last line in INTRINSIC_DOC_ATTRIBUTES contains undocumented attributes
+
+  DEFAULT_DOC_ATTRIBUTES = {
+    "attribute-missing" => "skip",
+    "attribute-undefined" => "drop-line",
+    "appendix-caption" => "Appendix",
+    "appendix-refsig" => "Appendix",
+    "caution-caption" => "Caution",
+    "chapter-refsig" => "Chapter",
+    "example-caption" => "Example",
+    "figure-caption" => "Figure",
+    "important-caption" => "Important",
+    "last-update-label" => "Last updated",
+    "note-caption" => "Note",
+    "part-refsig" => "Part",
+    "section-refsig" => "Section",
+    "table-caption" => "Table",
+    "tip-caption" => "Tip",
+    "toc-title" => "Table of Contents",
+    "untitled-label" => "Untitled",
+    "version-label" => "Version",
+    "warning-caption" => "Warning",
+    "doctype" => "article",
+    "prewrap" => "",
+    "sectids" => "",
+    "toc-placement" => "auto", # undocumented
+    "notitle" => "", # incorrectly documented
+    "max-include-depth" => 64,
+    "max-attribute-value-size" => 4096,
+    "linkcss" => "", # incorrectly documented
+    "stylesdir" => "."
+  }
+
+  register_for MY_BACKEND.to_sym
 
   def initialize(backend, opts = {})
     @backend = backend
-    init_backend_traits basebackend: 'asciidoc', filetype: 'asciidoc', outfilesuffix: '.adoc'
+    init_backend_traits basebackend: MY_BACKEND, filetype: MY_FILETYPE, outfilesuffix: MY_EXTENSION
   end
 
   def convert_document node
 
+    doctype = node.doctype
+
+    dynamic_exclusions = %W(
+      backend-#{MY_BACKEND}-doctype-#{doctype}
+      doctype-#{doctype}
+      backend-#{MY_BACKEND}
+      filetype-#{MY_FILETYPE}
+      basebackend-#{MY_BACKEND}-doctype-#{doctype}
+      basebackend-#{MY_BACKEND}
+    ).to_set
+
     result = []
     result << %(= #{node.title}) unless node.title.nil?
     node.attributes.each do |k,v|
-      result << %(:#{k}:#{v})
+      skip = -> {
+        INTRINSIC_DOC_ATTRIBUTES.include?(k) ||
+          dynamic_exclusions.include?(k) ||
+          DEFAULT_DOC_ATTRIBUTES[k] == v ||
+          (k == ATTR_DOC_TITLE && v == node.title) ||
+          (k == ATTR_ICONS_DIR && v == default_icons_dir(node))
+      }
+      result << %(:#{k}:#{v}) unless skip.call
     end
     result << ''
     node.blocks.each { |n| result << n.content }
@@ -39,9 +107,7 @@ class AsciiDoctorAsciiDocConverter < Asciidoctor::Converter::Base
 
   end
 
-  def convert_embedded node
-    ['', convert_document(node)].join LF
-  end
+  alias convert_embedded convert_document
 
   def convert_section node
 
@@ -217,6 +283,12 @@ class AsciiDoctorAsciiDocConverter < Asciidoctor::Converter::Base
   end
 
   private
+
+  def default_icons_dir node
+    images_dir = node.attributes[ATTR_IMAGES_DIR]
+    return './images/icons' if images_dir.nil? || "" == images_dir
+    "#{images_dir}/icons"
+  end
 
   def choose(*multiple)
     r = multiple.select { |p| !p.nil?}
